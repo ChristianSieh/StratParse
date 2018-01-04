65785223368a5447d145882c8fe7fe229bbbe434
christiansieh <christian.sieh@hotmail.com>
2018-01-04 10:07:20 -0700

    Broke up log file, added parsing for the header and beginning class implementation

--- logClass.py
+++ logClass.py
@@ -1,2 +1,10 @@
 class logClass:
-	
+	action = ""
+	headerType = ""
+	headerSubType = ""
+	data = ""
+
+	def printTestLog(self):
+		print("Action: ", self.action, type(self.action))
+		print("Type: ", self.headerType, type(self.headerType))
+		print("SubType: ", self.headerSubType, type(self.headerSubType))
--- parser.py
+++ parser.py
@@ -1,8 +1,45 @@
 import json
 import logClass
 
-with open("Sample-stratasys-status-log.txt", 'r') as log:
-	data = log.read()
-log.closed
+def readFile(filename):
+	with open(filename, 'r') as logfile:
+		data = logfile.read()
+	logfile.closed
 
-print(data)
+	logs = data.split(';')
+
+	return logs
+
+def parseLog(log):
+	myLog = logClass.logClass()
+
+	bodies = log.split("{\n")
+	#TODO strip bodies of ending }
+
+	#Get the header line with everything split apart
+	header = bodies[0].strip().split(' ')
+
+	#Assign action, header type, and header subtype
+	myLog.action = header[0]
+	temp = header[1].split('(')
+	myLog.headerType = temp[0]
+	myLog.headerSubType = temp[1].replace(')', '')
+
+	return myLog
+
+#Read file
+logs = readFile("Sample-stratasys-status-log.txt")
+
+#TODO fix extra log entry or just use pop
+logs.pop()
+
+logList = []
+
+#Parse each log in logs
+for log in logs:
+	logList.append(parseLog(log))
+
+#Print logs
+for log in logList:
+	log.printTestLog()
+
